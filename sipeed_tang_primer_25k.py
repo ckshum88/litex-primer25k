a4f0e05d2efc9f10029d225bda0a810e64f6ded7
Alfred Shum <ckshum@shums.com>
2026-06-07 20:26:22 +0100

    First commit

---
+++ sipeed_tang_primer_25k.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python3
+
+#
+# This file is part of LiteX-Boards.
+#
+# Copyright (c) 2023 Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Modified by Alfred Shum (https://github.com/ckshum88) on 7/6/2026
+
+from migen import *
+
+from litex.gen import *
+
+from litex.build.io import DDROutput
+
+from litex.soc.cores.clock.gowin_gw5a import GW5APLL
+from litex.soc.integration.soc import *
+from litex.soc.integration.builder import *
+from litex.soc.cores.led import LedChaser
+from litex.soc.cores.gpio import GPIOIn, GPIOOut
+
+from litedram.modules import AS4C32M16, W9825G6KH6
+from litedram.phy import GENSDRPHY, HalfRateGENSDRPHY
+
+from platforms import sipeed_tang_primer_25k
+
+# CRG ----------------------------------------------------------------------------------------------
+
+class _CRG(LiteXModule):
+    def __init__(self, platform, sys_clk_freq, with_sdram=False, sdram_rate="1:2"):
+        self.rst    = Signal()
+        self.cd_sys = ClockDomain()
+        self.cd_por = ClockDomain()
+
+        if with_sdram:
+            if sdram_rate == "1:2":
+                self.cd_sys2x    = ClockDomain()
+                self.cd_sys2x_ps = ClockDomain()
+            else:
+                self.cd_sys_ps = ClockDomain()
+
+        # # #
+
+        # Clk/Rst
+        clk50 = platform.request("clk50")
+        rst = platform.request("btn_n", 0)
+        self.comb += self.rst.eq(rst)
+
+        # Power on reset
+        por_count = Signal(16, reset=2**16-1)
+        por_done  = Signal()
+        self.comb += [
+            self.cd_por.clk.eq(clk50),
+            por_done.eq(por_count == 0),
+        ]
+        self.sync.por += If(~por_done, por_count.eq(por_count - 1))
+
+        # PLL
+        self.pll = pll = GW5APLL(devicename=platform.devicename, device=platform.device)
+        self.comb += pll.reset.eq(~por_done | self.rst)
+        pll.register_clkin(clk50, 50e6)
+        pll.create_clkout(self.cd_sys, sys_clk_freq)
+
+        # SDRAM clock
+        if with_sdram:
+            if sdram_rate == "1:2":
+                pll.create_clkout(self.cd_sys2x,    2*sys_clk_freq)
+                pll.create_clkout(self.cd_sys2x_ps, 2*sys_clk_freq, phase=180)
+                sdram_clk = ClockSignal("sys2x_ps")
+            else:
+                pll.create_clkout(self.cd_sys_ps, sys_clk_freq, phase=90)
+                sdram_clk = ClockSignal("sys_ps")
+            self.specials += DDROutput(1, 0, platform.request("sdram_clock"), sdram_clk)
+
+# BaseSoC ------------------------------------------------------------------------------------------
+
+class BaseSoC(SoCCore):
+    def __init__(self, toolchain="gowin", sys_clk_freq=50e6,
+        with_spi_flash  = False,
+        with_led_chaser = True,
+        with_buttons    = True,
+        with_sdram      = False,
+        sdram_model     = "sipeed",
+        sdram_rate      = "1:2",
+        **kwargs):
+
+        platform = sipeed_tang_primer_25k.Platform(toolchain=toolchain)
+
+        assert not with_sdram or (sdram_model in ["sipeed", "mister"])
+
+        if with_sdram:
+            platform.add_extension({
+                "sipeed": sipeed_tang_primer_25k.sipeedSDRAM(),
+                "mister": sipeed_tang_primer_25k.misterSDRAM}[sdram_model]
+            )
+
+        # CRG --------------------------------------------------------------------------------------
+        self.crg = _CRG(platform, sys_clk_freq, with_sdram, sdram_rate)
+
+        # SoCCore ----------------------------------------------------------------------------------
+        SoCCore.__init__(self, platform, sys_clk_freq, ident="LiteX SoC on Tang Primer 25K", **kwargs)
+
+        # SDR SDRAM --------------------------------------------------------------------------------
+        if with_sdram and not self.integrated_main_ram_size:
+            module_cls = {
+                "sipeed": W9825G6KH6,
+                "mister": AS4C32M16}[sdram_model]
+            if sdram_rate == "1:2":
+                sdrphy_cls = HalfRateGENSDRPHY
+            else:
+                sdrphy_cls = GENSDRPHY
+            self.sdrphy = sdrphy_cls(platform.request("sdram"), sys_clk_freq)
+            self.add_sdram("sdram",
+                phy           = self.sdrphy,
+                module        = module_cls(sys_clk_freq, sdram_rate),
+                l2_cache_size = kwargs.get("l2_size", 8192)
+            )
+
+        # SPI Flash --------------------------------------------------------------------------------
+        if with_spi_flash:
+            from litespi.modules import W25Q64FV as SpiFlashModule
+            from litespi.opcodes import SpiNorFlashOpCodes as Codes
+            self.add_spi_flash(mode="1x", module=SpiFlashModule(Codes.READ_1_1_1))
+
+        # Leds -------------------------------------------------------------------------------------
+        if with_led_chaser:
+            platform.add_extension(sipeed_tang_primer_25k.pmod_led())
+            self.leds = LedChaser(
+                pads         = platform.request_all("pmod_led"),
+                sys_clk_freq = sys_clk_freq,
+                polarity     = 1
+            )
+
+        # Buttons ----------------------------------------------------------------------------------
+        if with_buttons:
+            self.buttons = GPIOIn(pads=platform.request("btn_n", 1))
+
+        # Pmods ------------------------------------------------------------------------------------
+        platform.add_extension(sipeed_tang_primer_25k.pmod_dt())
+        self.pmod_dt = GPIOOut(pads=platform.request("pmod_dt"))
+        platform.add_extension(sipeed_tang_primer_25k.pmod_btn())
+        self.pmod_btn = GPIOIn(pads=~platform.request_all("pmod_btn"))
+
+# Build --------------------------------------------------------------------------------------------
+
+def main():
+    from litex.build.parser import LiteXArgumentParser
+    parser = LiteXArgumentParser(platform=sipeed_tang_primer_25k.Platform, description="LiteX SoC on Tang Primer 25K.")
+    parser.add_target_argument("--flash",            action="store_true",      help="Flash bitstream.")
+    parser.add_target_argument("--sys-clk-freq",     default=50e6, type=float, help="System clock frequency.")
+    parser.add_target_argument("--with-spi-flash",   action="store_true",      help="Enable memory-mapped SPI flash.")
+    parser.add_target_argument("--with-sdram",       action="store_true",      help="Enable optional SDRAM module.")
+    parser.add_target_argument("--sdram-model",      default="sipeed",
+        choices=[
+            "sipeed",
+            "mister"
+    ], help="SDRAM module model.")
+    args = parser.parse_args()
+
+    soc = BaseSoC(
+        toolchain      = args.toolchain,
+        sys_clk_freq   = args.sys_clk_freq,
+        with_spi_flash = args.with_spi_flash,
+        with_sdram     = args.with_sdram,
+        sdram_model    = args.sdram_model,
+        **parser.soc_argdict
+    )
+    builder = Builder(soc, **parser.builder_argdict)
+    if args.build:
+        builder.build(**parser.toolchain_argdict)
+
+    if args.load:
+        prog = soc.platform.create_programmer()
+        prog.load_bitstream(builder.get_bitstream_filename(mode="sram"))
+
+if __name__ == "__main__":
+    main()
